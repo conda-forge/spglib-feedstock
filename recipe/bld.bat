33506cbecedd2ef83d3847cd64d168cd6035aa27
Paweł T. Jochym <jochym@users.noreply.github.com>
2018-11-20 00:24:10 +0100

    Switch to pip for win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,8 @@
+md python\src
+copy src\*.* python\src
 cd python
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt 
+"%PYTHON%" -m pip install .
+:: "%PYTHON%" setup.py install --single-version-externally-managed --record record.txt 
 if errorlevel 1 exit 1
 
 :: No library build on windows!
