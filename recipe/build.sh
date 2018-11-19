b905fecb0a12f2dd341513ecefc60392bb82f56a
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2018-11-20 00:04:00 +0100

    Switch to pip for install in windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,8 @@ cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_INSTALL_PREFIX=$PREFIX ${SRC_DIR}
 make install
 
 (cd python
+    cp -r ../src .
     $PYTHON -m pip install .
-    #$PYTHON setup.py install --single-version-externally-managed --record record.txt 
 )
 
 
