55ae494beec7bde961bde782b0892d96efbd0b4e
Paweł T. Jochym <jochym@users.noreply.github.com>
2018-11-20 08:02:31 +0100

    Clean up

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,6 @@ md python\src
 copy src\*.* python\src
 cd python
 "%PYTHON%" -m pip install .
-:: "%PYTHON%" setup.py install --single-version-externally-managed --record record.txt 
 if errorlevel 1 exit 1
 
 :: No library build on windows!
