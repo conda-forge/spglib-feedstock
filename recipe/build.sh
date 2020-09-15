709ac761a602d9bd6d270212fc9f94d091189d3e
Paweł T. Jochym <jochym@users.noreply.github.com>
2020-09-15 16:30:30 +0200

    Clean-up build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,6 @@
 cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib ${SRC_DIR}
 make install
 
-ls -lR $PREFIX/lib
-
 (cd python
     cp -r ../src .
     $PYTHON -m pip install .
