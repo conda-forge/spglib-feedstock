81c4dd30c44101f36e99a7f42d85ced57481dce2
Paweł T. Jochym <jochym@users.noreply.github.com>
2020-09-15 16:00:04 +0200

    Install to $PREFIX/lib

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-echo $CC $PREFIX $SRC_DIR
+echo $CC 
+echo $PREFIX 
+echo $SRC_DIR
 
 cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_INSTALL_PREFIX=$PREFIX ${SRC_DIR}
-make install
+make 
+make DESTDIR=$PREFIX/lib install
+
+ls -l $PREFIX/lib
 
 (cd python
     cp -r ../src .
     $PYTHON -m pip install .
 )
+
 
 
